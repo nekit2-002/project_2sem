f7ba54c8f05ad12bdde88978d766a5a5128a9237
Svyatoslav2 <20020511@bk.ru>
2021-02-09 15:29:57 +0300

    Create Project1.cpp

---
+++ Project1.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int disq(int a, int b, int c)
+{
+	int d = b*b - 4 * a*c;
+	return d;
+}
+
+
+
+
+int main()
+{
+	int a, b, c;
+	scanf_s("%d",&a);
+	scanf_s("%d",&b );
+	scanf_s("%d",&c);
+
+	int f = disq(a, b, c);
+	if (f >= 0)
+	{
+		double x1 = (-b + sqrt(f)) / (2 * a);
+		double x2 = (-b - sqrt(f)) / (2 * a);
+		printf("%f \n", x1);
+		printf("%f \n", x2);
+	}
+	else
+	{
+		printf("Solution isnt exist");
+	}
+	
+	system("pause");
+	return 0;
+}
