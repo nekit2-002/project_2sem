c85e8a8113ee23aaf1d5be6534b26e41ef90cd13
Nikita Kosenko <foxidalim@gmail.com>
2021-04-03 22:46:43 +0300

    Testing

--- Project1.cpp
+++ Project1.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-
+//clejjevve
 
 int disq(int a, int b, int c)
 {
--- help.py
+++ help.py
@@ -1,4 +1,4 @@
-# 0 1 1 2 3 5 8 13 21
+# 0 1 1 2 3 5 8 13 21 3fnjnevveojfe
 def foo(pos):
     if pos < 2:
         return pos
