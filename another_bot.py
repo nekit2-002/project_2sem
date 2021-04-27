9dcf7d894b7ec25c8a08cad7fd9d32d94e2df506
Nikita Kosenko <foxidalim@gmail.com>
2021-04-28 00:37:03 +0300

    Choose the repository

--- another_bot.py
+++ another_bot.py
@@ -14,6 +14,8 @@ bot = telegram.Bot(token=TG_TOKEN)
 dispatcher = telegram.Dispatcher(bot)
 
 subscribers = {}
+repositories_full = ['nekit2-002/dummy', 'nekit2-002/dummy2']
+repositories_to_listen = []
 
 
 @dispatcher.message_handler()
@@ -27,7 +29,28 @@ async def handle_message(message):
         await message.reply(parse_mode='Markdown',
                             text='Subscribing to all repositories')
 
-        subscribers[username] = chat
+        repositories_to_listen = repositories_full
+
+        subscribers[username] = (chat, repositories_to_listen)
+
+    async def listen_to():
+        await bot.send_message(chat, parse_mode='Markdown', text='''
+        Choose the repository you want to be informed about!
+        ''')
+        await bot.send_message(chat, parse_mode='Markdown',
+                               text=f'{repositories_full}')
+
+        response = message.text
+
+        for repository in range(len(repositories_full)):
+            if response == repository:
+                await bot.send_message(chat, parse_mode='Markdown', text=f'''
+                You have chosen the repository {repository} to listen to.
+                ''')
+
+                repositories_to_listen.append(f'{repository}')
+
+                subscribers[username] = (chat, repositories_to_listen)
 
     async def do_cancel():
         await message.reply(parse_mode='Markdown',
@@ -46,15 +69,17 @@ async def handle_message(message):
         await message.reply(parse_mode='Markdown', text=reply)
 
     actions = {
-        "listen": do_listen,
+        'listen': do_listen,
         "cancel": do_cancel,
+        'listen_to': listen_to
     }
-
     action = actions.get(message.text, do_error)
     await action()
 
 
 routes = web.RouteTableDef()
+keys = subscribers.keys()
+values = list(subscribers.values())
 
 
 @routes.post('/github_events')
@@ -76,10 +101,16 @@ async def handle_github(request):
             *{user}* has pushed [{commit_hash}]({commit_url}) to `{branch}`.
             Repository: [{repo_name}]({repo_url}).
         ''')
-        logging.info(message)
 
-        for chat in subscribers.values():
-            await bot.send_message(chat, parse_mode='Markdown', text=message)
+        logging.info(message)
+        for i in range(len(subscribers)):
+            for chat in values[i][0]:
+                for name in range(len(keys)):
+                    for rep in range(len(repositories_to_listen)):
+                        if repo_name == rep:
+                            await bot.send_message(chat,
+                                                   parse_mode='Markdown',
+                                                   text=message)
 
     # Reply with 200 OK
     return web.Response()
