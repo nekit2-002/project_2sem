3a164ee89eabd31a9e84e195a199d6f900d27f66
Nikita Kosenko <foxidalim@gmail.com>
2021-05-10 20:56:22 +0300

    Deleting some rubbish

--- help.py
+++
@@ -1,9 +0,0 @@
-# 0 1 1 2 3 5 8 13 21 3fnckmeeejie
-def foo(pos):
-    if pos < 2:
-        return pos
-    else:
-        return foo(pos - 1) + foo(pos - 2)
-
-
-print(foo(int(input("Введите число -> "))))
